d8f72e0ad2d3be3473b7862d2f45f9d6a1d23b91
TopDev <kwangxu0924@gmail.com>
2023-04-18 19:58:59 +0200

    Added logo and welcome page image

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 
 Route::post('get-states-by-country', 'CountryStateCityController@getState');
