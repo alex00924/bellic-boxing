f87e119f3d0738ad3d4d9eb35148f2f9c20fcdc0
TopDev <kwangxu0924@gmail.com>
2023-04-20 22:57:14 +0200

    updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,21 +14,31 @@
 
     </head>
     <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+        <div class="relative min-h-screen dark:bg-gray-900 selection:bg-red-500 selection:text-white bg-contain bg-no-repeat"
+            style="background-image: url(/images/home.png)">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="pt-7 h-16 text-right">
+                    <span class="inline-flex items-center px-3 text-2xl pb-1   dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+                        Boxers
+                    </span>
+                    <span class="inline-flex items-center px-3 text-2xl pb-1   dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+                        Events
+                    </span>
+                    <span class="inline-flex items-center px-3 text-2xl pb-1   dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+                        Matchmaking
+                    </span>
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="font-semibold inline-flex items-center px-3 text-2xl pb-1   dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
+                        <a href="{{ route('register') }}" class="font-semibold inline-flex items-center px-3 text-2xl pb-1   dark:text-red-500 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">Join Now</a>
+                        <a href="{{ route('login') }}" class="bg-red-500 font-semibold inline-flex items-center px-3 text-2xl pb-1   dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">Log in</a>
                     @endauth
                 </div>
-            @endif
-            <img src="/images/home.png" class="w-full">
+                <img src="/images/logo.png" class="h-20 fixed top-5 left-12" />
+                <span class="fixed bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl text-gray-300"> 
+                    Bellic Boxing
+                </span>
+            </div>
         </div>
     </body>
 </html>
